c8d5056067b8c7980fd7a8ca1e5916e053f92eaa
Jhonatan Agreda Ch. <152299898+JhonatanAC20@users.noreply.github.com>
2024-12-12 10:03:12 -0500

    cambios

--- app/Views/principal/index.php
+++ app/Views/principal/index.php
@@ -1,37 +1,40 @@
 <?= $this->extend('include/layout.php'); ?>
 
 <?= $this->section('contenido'); ?>
+
 <body>
     <br>
-    <!-- Carrusel con Bootstrap -->
-    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="resources/img/sepisac.png" class="d-block w-100" alt="Imagen 1">
-            </div>
-            <div class="carousel-item">
-                <img src="resources/img/comercializacion.png" class="d-block w-100" alt="Imagen 2">
-            </div>
-            <div class="carousel-item">
-                <img src="resources/img/fabricacion.png" class="d-block w-100" alt="Imagen 3">
-            </div>
-        </div>
-        <!-- Controles del carrusel -->
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">anterior</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">siguiente</span>
-        </button>
-    </div>
-    <br>
-    </br>
-
     <!-- contenido  -->
     <div class="content-section">
         <div class="container">
+            <!-- Carrusel con Bootstrap -->
+            <div class="row">
+                <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="resources/img/sepisac.png" class="d-block w-100" alt="Imagen 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="resources/img/comercializacion.png" class="d-block w-100" alt="Imagen 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="resources/img/fabricacion.png" class="d-block w-100" alt="Imagen 3">
+                        </div>
+                    </div>
+                    <!-- Controles del carrusel -->
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">anterior</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">siguiente</span>
+                    </button>
+                </div>
+                <br>
+                </br>
+            </div>
+            
             <div class="row">
                 <div class="col-md-4 grid-item">
                     <img src="resources/img/climatizacion.png" alt="Producto 1">
@@ -52,4 +55,4 @@
         </div>
     </div>
 
-<?= $this->endSection(); ?>
+    <?= $this->endSection(); ?>
