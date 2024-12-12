08814263d49c866f23ea123d519a208f5e789f4d
Jhonatan Agreda Ch. <152299898+JhonatanAC20@users.noreply.github.com>
2024-12-12 11:23:19 -0500

    mejoras

--- app/Views/include/head.php
+++ app/Views/include/head.php
@@ -10,40 +10,38 @@
     <link rel="stylesheet" href="resources/css/styles.css" type="text/css">
 </head>
 
-<header>
-    <!-- Barra de navegación -->
-    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container-fluid"> <!--logo-->
-            <a class="navbar-brand" href="../pagina/inicio.php">
-                <img src="resources/img/SEPII.png" alt="Logo de la Empresa" width="160"> <!-- Ajusta el tamaño del logo según sea necesario -->
-                <div class="line"></div>
-            </a>
+<body>
 
-            <!-- Botón de menú-->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div class="content-section">
+        <div class="container-fluid">
+            <header>
+                <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <a class="navbar-brand" href="../pagina/inicio.php">
+                        <img src="resources/img/SEPII.png" alt="Logo de la Empresa" width="160"> <!-- Ajusta el tamaño del logo según sea necesario -->
+                        <div class="line"></div>
+                    </a>
 
-            <!-- Elementos del menú -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active link-principal" aria-current="page"><i class='bx bxs-home'></i> Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../pagina/nosotros.php"><i class='bx bxs-group' ></i> Nosotros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../pagina/servicios.php"><i class='bx bxs-wrench'></i> Servicios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../pagina/inicio.php"><i class='bx bxs-phone-call' ></i> Contactos</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <br>
-    </br>
-</header>
-<br>
+                    <!-- Botón de menú-->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <!-- Elementos del menú -->
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active link-principal" aria-current="page"><i class='bx bxs-home'></i> Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../pagina/nosotros.php"><i class='bx bxs-group'></i> Nosotros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../pagina/servicios.php"><i class='bx bxs-wrench'></i> Servicios</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../pagina/inicio.php"><i class='bx bxs-phone-call'></i> Contactos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
--- app/Views/principal/index.php
+++ app/Views/principal/index.php
@@ -2,14 +2,10 @@
 
 <?= $this->section('contenido'); ?>
 
-<body>
-    <br>
-    <!-- contenido  -->
-    <div class="content-section">
-        <div class="container">
-            <!-- Carrusel con Bootstrap -->
-            <div class="row">
-                <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+<main>
+    <!-- Carrusel con Bootstrap -->
+    <div class="row">
+        <!--<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="resources/img/sepisac.png" class="d-block w-100" alt="Imagen 1">
@@ -20,9 +16,9 @@
                         <div class="carousel-item">
                             <img src="resources/img/fabricacion.png" class="d-block w-100" alt="Imagen 3">
                         </div>
-                    </div>
-                    <!-- Controles del carrusel -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    </div> -->
+        <!-- Controles del carrusel -->
+        <!--    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">anterior</span>
                     </button>
@@ -33,9 +29,23 @@
                 </div>
                 <br>
                 </br>
+                -->
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="resources/img/carrusel_principal/SEPI_2019.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="resources/img/carrusel_principal/SEPI_2024.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="resources/img/carrusel_principal/Fresas.jpg" class="d-block w-100" alt="...">
+                </div>
             </div>
-            
-            <div class="row">
+        </div>
+    </div>
+
+    <!--<div class="row">
                 <div class="col-md-4 grid-item">
                     <img src="resources/img/climatizacion.png" alt="Producto 1">
                     <h5>Climatización</h5>
@@ -51,8 +61,9 @@
                     <h5>Fabricación de Maquinas</h5>
                     <p>Somos lideres en la fabricación de maquinaria industrial de alta calidad, especializada en satisfacer las demandas mas exigentes de los sectores de construcción.</p>
                 </div>
-            </div>
-        </div>
-    </div>
+            </div>-->
+</main>
+</div>
+</div>
 
-    <?= $this->endSection(); ?>
+<?= $this->endSection(); ?>
