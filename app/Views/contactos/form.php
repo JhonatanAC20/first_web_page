48d502027f5a4c0bb139ad6c3d5e8101dc555a2c
Jhonatan Agreda Ch. <152299898+JhonatanAC20@users.noreply.github.com>
2024-12-13 14:54:40 -0500

    agregar formulario

--- app/Views/contactos/form.php
+++ app/Views/contactos/form.php
@@ -3,7 +3,20 @@
 <?= $this->section('contenido'); ?>
 
 <main class="vw-contactos">
-    <h5>Formulario</h5>
+    <div class="row">
+        <div class="columna col-12">
+            <div class="widget nueva_entrada">
+                <h3 class="titulo">Nuevo Mensaje</h3>
+                <form action="">
+                    <input type="text" placeholder="Titulo de la entrada" />
+                    <textarea placeholder="Lorem ipsum dolor sit amet consectetur"></textarea>
+                    <div class="d-flex justify-content-end">
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </main>
 </div>
 </div>
--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -6,56 +6,56 @@ $(document).ready(function () {
     var logo = document.getElementsByClassName('navbar-brand')[0].getElementsByTagName('img')[0];
     logo.draggable = false;
 
-    if($('.vw-principal').length){
+    if ($('.vw-principal').length) {
 
         const $link_principal = $('.link-principal');
-        
+
         $link_principal[0].style.setProperty('color', '#ff0c0c', 'important');
-        
-        $('.line-select-principal').css({'display':'block'});
+
+        $('.line-select-principal').css({ 'display': 'block' });
     }
 
-    if($('.vw-nosotros').length){
+    if ($('.vw-nosotros').length) {
 
         const $link_nosotros = $('.link-nosotros');
-        
+
         $link_nosotros[0].style.setProperty('color', '#ff0c0c', 'important');
-        
-        $('.line-select-nosotros').css({'display':'block'});
+
+        $('.line-select-nosotros').css({ 'display': 'block' });
     }
-    
-    if($('.vw-servicios').length){
+
+    if ($('.vw-servicios').length) {
         const $link_servicios = $('.link-servicios');
 
         $link_servicios[0].style.setProperty('color', '#ff0c0c', 'important');
 
-        $('.line-select-servicios').css({'display':'block'});
+        $('.line-select-servicios').css({ 'display': 'block' });
     }
 
-    if($('.vw-contactos').length){
+    if ($('.vw-contactos').length) {
         const $link_contactos = $('.link-contactos');
 
         $link_contactos[0].style.setProperty('color', '#ff0c0c', 'important');
 
-        $('.line-select-contactos').css({'display':'block'});
+        $('.line-select-contactos').css({ 'display': 'block' });
     }
 
-    $('.link-principal').click(function(e){
+    $('.link-principal').click(function (e) {
         e.preventDefault();
         goLink('principal');
     })
 
-    $('.link-nosotros').click(function(e){
+    $('.link-nosotros').click(function (e) {
         e.preventDefault();
         goLink('nosotros');
     })
 
-    $('.link-servicios').click(function(e){
+    $('.link-servicios').click(function (e) {
         e.preventDefault();
         goLink('servicios');
     })
 
-    $('.link-contactos').click(function(e){
+    $('.link-contactos').click(function (e) {
         e.preventDefault();
         goLink('contactos');
     })
@@ -67,5 +67,5 @@ $(document).ready(function () {
         var url = `http://${dominio}:${puerto}/first_web_page/public/${view}`;
 
         $(location).attr('href', url);
-    }   
+    }
 })
